51c35015ad90d06793ebbea4347ed9765c9fd588
Davyd McColl <davydm@gmail.com>
2025-03-10 15:52:16 +0200

    :sparkles: add babel support when requesting jest
    - resolves issues with, eg, using octokit

--- src/spawn.ts
+++ src/spawn.ts
@@ -1,62 +1,69 @@
 import { spawn as _spawn, SpawnOptions } from "child_process";
 
 export function spawn(
-    command: string,
-    args?: string[],
-    options?: SpawnOptions
+  command: string,
+  args?: string[],
+  options?: SpawnOptions
 ): Promise<ProcessData> {
+  const
+    spawnArgs = args || [] as string[],
+    spawnOptions = options || {} as SpawnOptions;
+  return new Promise<ProcessData>((resolve, reject) => {
     const
-        spawnArgs = args || [] as string[],
-        spawnOptions = options || {} as SpawnOptions;
-    return new Promise<ProcessData>((resolve, reject) => {
-        const
-            child = _spawn(command, spawnArgs, spawnOptions),
-            result: ProcessData = {
-                stdout: [],
-                stderr: [],
-                code: -1
-            };
-        child.stderr?.on("data", d => {
-            result.stderr.push(d.toString());
-        })
-        child.stdout?.on("data", d => {
-            result.stdout.push(d.toString());
-        });
-        child.on("close", code => {
-            result.code = code;
-            return code
-                ? reject(makeSpawnErrorFor(result, command, args, options))
-                : resolve(result);
-        });
+      child = _spawn(command, spawnArgs, spawnOptions),
+      result: ProcessData = {
+        stdout: [],
+        stderr: [],
+        code: -1
+      };
+    child.stderr?.on("data", d => {
+      result.stderr.push(d.toString());
+    })
+    child.stdout?.on("data", d => {
+      result.stdout.push(d.toString());
     });
+    child.on("close", code => {
+      if (code === null) {
+        return reject(makeSpawnErrorFor(
+            result, command, args, options
+          )
+        )
+      }
+      result.code = code;
+      return code
+        ? reject(makeSpawnErrorFor(result, command, args, options))
+        : resolve(result);
+    });
+  });
 }
 
 function makeSpawnErrorFor(
-    result: ProcessData,
-    command: string,
-    args: string[] | undefined,
-    options: SpawnOptions | undefined
+  result: ProcessData,
+  command: string,
+  args: string[] | undefined,
+  options: SpawnOptions | undefined
 ): SpawnError {
-    const e = new Error(`Error exit code ${result.code} for: ${command} ${(args || []).join(" ")}`);
-    return {
-        ...e,
-        result,
-        command,
-        args,
-        options
-    };
+  const e = new Error(`Error exit code ${ result.code } for: ${ command } ${ (args || []).join(" ") }`);
+  return {
+    ...e,
+    result,
+    command,
+    args,
+    options
+  };
 }
 
-export interface SpawnError extends Error {
-    command: string;
-    args: string[] | undefined;
-    options: SpawnOptions | undefined,
-    result: ProcessData;
+export interface SpawnError
+  extends Error {
+  command: string;
+  args: string[] | undefined;
+  options: SpawnOptions | undefined,
+  result: ProcessData;
 }
 
 export interface ProcessData {
-    stdout: string[];
-    stderr: string[];
-    code: number;
+  stdout: string[];
+  stderr: string[];
+  code: number;
 }
 
--- tests/npm.install.spec.ts
+++ tests/npm.install.spec.ts
@@ -5,223 +5,327 @@ import * as faker from "faker";
 import { Sandbox } from "filesystem-sandbox";
 
 describe(`install required packages`, () => {
-    describe(`tslint`, () => {
-        it(`should install tslint by default`, async () => {
-            // Arrange
-            const
-                name = faker.random.alphaNumeric(5),
-                sandbox = await Sandbox.create(),
-                where = sandbox.path;
-            // Act
-            await runTsBoot({
-                name, where
-            })
-            // Assert
-            expect(shared.spawnModule)
-                .toHaveInstalledDevDependency("tslint");
-        });
-
-        it(`should skip tslint on request`, async () => {
-            // Arrange
-            const { name, where } = await init();
-            // Act
-            await runTsBoot({
-                name,
-                where,
-                includeLinter: false
-            })
-            // Assert
-            expect(shared.spawnModule)
-                .not.toHaveInstalledDevDependency("tslint");
-        });
+  describe(`tslint`, () => {
+    it(`should install tslint by default`, async () => {
+      // Arrange
+      const
+        name = faker.random.alphaNumeric(5),
+        sandbox = await Sandbox.create(),
+        where = sandbox.path;
+      // Act
+      await runTsBoot({
+        name,
+        where
+      })
+      // Assert
+      expect(shared.spawnModule)
+        .toHaveInstalledDevDependency("tslint");
     });
 
-    it(`should install typescript`, async () => {
-        // Arrange
-        const
-            name = faker.random.alphaNumeric(5),
-            sandbox = await Sandbox.create(),
-            where = sandbox.path;
-        // Act
-        await runTsBoot({
+    it(`should skip tslint on request`, async () => {
+      // Arrange
+      const {
+        name,
+        where
+      } = await init();
+      // Act
+      await runTsBoot({
+        name,
+        where,
+        includeLinter: false
+      })
+      // Assert
+      expect(shared.spawnModule)
+        .not.toHaveInstalledDevDependency("tslint");
+    });
+  });
+
+  it(`should install typescript`, async () => {
+    // Arrange
+    const
+      name = faker.random.alphaNumeric(5),
+      sandbox = await Sandbox.create(),
+      where = sandbox.path;
+    // Act
+    await runTsBoot({
+      name,
+      where
+    });
+    // Assert
+    expect(shared.spawnModule)
+      .toHaveInstalledDevDependency("typescript");
+  });
+
+  describe(`@node/types`, () => {
+    it(`should install @types/node by default`, async () => {
+      // Arrange
+      const {
+        name,
+        where
+      } = await init();
+      // Act
+      await runTsBoot({
+        name,
+        where
+      })
+      // Assert
+      expect(shared.spawnModule)
+        .toHaveInstalledDevDependency("@types/node");
+    });
+
+    it(`should skip @types/node on request`, async () => {
+      // Arrange
+      const {
+        name,
+        where
+      } = await init();
+      // Act
+      await runTsBoot({
+        name,
+        where,
+        includeNodeTypes: false
+      })
+      // Assert
+      expect(shared.spawnModule)
+        .not.toHaveInstalledDevDependency("@types/node");
+    });
+  });
+  // faker
+  describe(`faker`, () => {
+    it(`should install @faker-js/faker by default`, async () => {
+      // Arrange
+      const {
+        name,
+        where
+      } = await init();
+      // Act
+      await runTsBoot({
+        name,
+        where
+      })
+      // Assert
+      expect(shared.spawnModule)
+        .toHaveInstalledDevDependency("@faker-js/faker");
+    });
+    it(`should skip faker on request`, async () => {
+      // Arrange
+      const {
+        name,
+        where
+      } = await init();
+      // Act
+      await runTsBoot({
+        name,
+        where,
+        includeFaker: false
+      })
+      // Assert
+      expect(shared.spawnModule)
+        .not.toHaveInstalledDevDependency("faker");
+      expect(shared.spawnModule)
+        .not.toHaveInstalledDevDependency("@types/faker");
+    });
+  });
+  describe(`jest`, () => {
+    it(`should install jest by default`, async () => {
+      // Arrange
+      const {
+        name,
+        where
+      } = await init();
+      // Act
+      await runTsBoot({
+        name,
+        where
+      })
+      // Assert
+      expect(shared.spawnModule)
+        .toHaveInstalledDevDependency("jest");
+    });
+    it(`should skip jest on request`, async () => {
+      // Arrange
+      const {
+        name,
+        where
+      } = await init();
+      // Act
+      await runTsBoot({
+        name,
+        where,
+        includeJest: false
+      })
+      // Assert
+      expect(shared.spawnModule)
+        .not.toHaveInstalledDevDependency("jest");
+    });
+    describe(`babelification`, () => {
+      [ "@babel/preset-env", "babel-jest" ].forEach(pkg => {
+        it(`should install by default: ${ pkg }`, async () => {
+          // Arrange
+          const {
             name,
             where
-        });
-        // Assert
-        expect(shared.spawnModule)
-            .toHaveInstalledDevDependency("typescript");
-    });
-
-    describe(`@node/types`, () => {
-        it(`should install @types/node by default`, async () => {
-            // Arrange
-            const { name, where } = await init();
-            // Act
-            await runTsBoot({
-                name, where
-            })
-            // Assert
-            expect(shared.spawnModule)
-                .toHaveInstalledDevDependency("@types/node");
+          } = await init();
+          // Act
+          await runTsBoot({
+            name,
+            where
+          })
+          // Assert
+          expect(shared.spawnModule)
+            .toHaveInstalledDevDependency(pkg);
         });
 
-        it(`should skip @types/node on request`, async () => {
-            // Arrange
-            const { name, where } = await init();
-            // Act
-            await runTsBoot({
-                name, where, includeNodeTypes: false
-            })
-            // Assert
-            expect(shared.spawnModule)
-                .not.toHaveInstalledDevDependency("@types/node");
+        it(`should skip package when jest not required: ${pkg}`, async () => {
+          // Arrange
+          const {
+            name,
+            where
+          } = await init();
+          // Act
+          await runTsBoot({
+            name,
+            where,
+            includeJest: false
+          })
+          // Assert
+          expect(shared.spawnModule)
+            .not.toHaveInstalledDevDependency(pkg);
         });
+      });
     });
-    // faker
-    describe(`faker`, () => {
-        it(`should install @faker-js/faker by default`, async () => {
-            // Arrange
-            const { name, where } = await init();
-            // Act
-            await runTsBoot({
-                name, where
-            })
-            // Assert
-            expect(shared.spawnModule)
-                .toHaveInstalledDevDependency("@faker-js/faker");
-        });
-        it(`should skip faker on request`, async () => {
-            // Arrange
-            const { name, where } = await init();
-            // Act
-            await runTsBoot({
-                name, where, includeFaker: false
-            })
-            // Assert
-            expect(shared.spawnModule)
-                .not.toHaveInstalledDevDependency("faker");
-            expect(shared.spawnModule)
-                .not.toHaveInstalledDevDependency("@types/faker");
-        });
+  });
+  describe(`expect-even-more-jest`, () => {
+    it(`should install expect-even-more-jest by default`, async () => {
+      // Arrange
+      const {
+        name,
+        where
+      } = await init();
+      // Act
+      await runTsBoot({
+        name,
+        where
+      })
+      // Assert
+      expect(shared.spawnModule)
+        .toHaveInstalledDevDependency("expect-even-more-jest");
     });
-    describe(`jest`, () => {
-        it(`should install jest by default`, async () => {
-            // Arrange
-            const { name, where } = await init();
-            // Act
-            await runTsBoot({
-                name, where
-            })
-            // Assert
-            expect(shared.spawnModule)
-                .toHaveInstalledDevDependency("jest");
-        });
-        it(`should skip jest on request`, async () => {
-            // Arrange
-            const { name, where } = await init();
-            // Act
-            await runTsBoot({
-                name, where, includeJest: false
-            })
-            // Assert
-            expect(shared.spawnModule)
-                .not.toHaveInstalledDevDependency("jest");
-        });
+    it(`should skip expect-even-more-jest on request`, async () => {
+      // Arrange
+      const {
+        name,
+        where
+      } = await init();
+      // Act
+      await runTsBoot({
+        name,
+        where,
+        includeExpectEvenMoreJest: false
+      })
+      // Assert
+      expect(shared.spawnModule)
+        .not.toHaveInstalledDevDependency("expect-even-more-jest");
     });
-    describe(`expect-even-more-jest`, () => {
-        it(`should install expect-even-more-jest by default`, async () => {
-            // Arrange
-            const { name, where } = await init();
-            // Act
-            await runTsBoot({
-                name, where
-            })
-            // Assert
-            expect(shared.spawnModule)
-                .toHaveInstalledDevDependency("expect-even-more-jest");
-        });
-        it(`should skip expect-even-more-jest on request`, async () => {
-            // Arrange
-            const { name, where } = await init();
-            // Act
-            await runTsBoot({
-                name, where, includeExpectEvenMoreJest: false
-            })
-            // Assert
-            expect(shared.spawnModule)
-                .not.toHaveInstalledDevDependency("expect-even-more-jest");
-        });
+  });
+  describe(`rimraf`, () => {
+    it(`should install rimraf by default`, async () => {
+      // Arrange
+      const {
+        name,
+        where
+      } = await init();
+      // Act
+      await runTsBoot({
+        name,
+        where
+      })
+      // Assert
+      expect(shared.spawnModule)
+        .toHaveInstalledDevDependency("rimraf");
     });
-    describe(`rimraf`, () => {
-        it(`should install rimraf by default`, async () => {
-            // Arrange
-            const { name, where } = await init();
-            // Act
-            await runTsBoot({
-                name, where
-            })
-            // Assert
-            expect(shared.spawnModule)
-                .toHaveInstalledDevDependency("rimraf");
-        });
-        it(`should skip rimraf when no test script`, async () => {
-            // Arrange
-            const { name, where } = await init();
-            // Act
-            await runTsBoot({
-                name, where, setupTestScript: false
-            })
-            // Assert
-            expect(shared.spawnModule)
-                .not.toHaveInstalledDevDependency("rimraf");
-        });
+    it(`should skip rimraf when no test script`, async () => {
+      // Arrange
+      const {
+        name,
+        where
+      } = await init();
+      // Act
+      await runTsBoot({
+        name,
+        where,
+        setupTestScript: false
+      })
+      // Assert
+      expect(shared.spawnModule)
+        .not.toHaveInstalledDevDependency("rimraf");
     });
+  });
 
-    describe(`zarro`, () => {
-        it(`should install zarro by default`, async () => {
-            // Arrange
-            const { name, where } = await init();
-            // Act
-            await runTsBoot({
-                name, where
-            })
-            // Assert
-            expect(shared.spawnModule)
-                .toHaveInstalledDevDependency("zarro");
-        });
-        it(`should skip zarro on request`, async () => {
-            // Arrange
-            const { name, where } = await init();
-            // Act
-            await runTsBoot({
-                name, where, includeZarro: false
-            })
-            // Assert
-            expect(shared.spawnModule)
-                .not.toHaveInstalledDevDependency("zarro");
-        });
+  describe(`zarro`, () => {
+    it(`should install zarro by default`, async () => {
+      // Arrange
+      const {
+        name,
+        where
+      } = await init();
+      // Act
+      await runTsBoot({
+        name,
+        where
+      })
+      // Assert
+      expect(shared.spawnModule)
+        .toHaveInstalledDevDependency("zarro");
     });
+    it(`should skip zarro on request`, async () => {
+      // Arrange
+      const {
+        name,
+        where
+      } = await init();
+      // Act
+      await runTsBoot({
+        name,
+        where,
+        includeZarro: false
+      })
+      // Assert
+      expect(shared.spawnModule)
+        .not.toHaveInstalledDevDependency("zarro");
+    });
+  });
 
-    it(`should install cross-env`, async () => {
-        // Arrange
-        const { name, where } = await init();
-        // Act
-        await runTsBoot({
-            name, where, includeZarro: faker.random.boolean()
-        });
-        // Assert
-        expect(shared.spawnModule)
-            .toHaveInstalledDevDependency("cross-env");
+  it(`should install cross-env`, async () => {
+    // Arrange
+    const {
+      name,
+      where
+    } = await init();
+    // Act
+    await runTsBoot({
+      name,
+      where,
+      includeZarro: faker.random.boolean()
     });
-    it(`should install npm-run-all`, async () => {
-        // Arrange
-        const { name, where } = await init();
-        // Act
-        await runTsBoot({
-            name, where, includeZarro: faker.random.boolean()
-        });
-        // Assert
-        expect(shared.spawnModule)
-            .toHaveInstalledDevDependency("npm-run-all");
+    // Assert
+    expect(shared.spawnModule)
+      .toHaveInstalledDevDependency("cross-env");
+  });
+  it(`should install npm-run-all`, async () => {
+    // Arrange
+    const {
+      name,
+      where
+    } = await init();
+    // Act
+    await runTsBoot({
+      name,
+      where,
+      includeZarro: faker.random.boolean()
     });
+    // Assert
+    expect(shared.spawnModule)
+      .toHaveInstalledDevDependency("npm-run-all");
+  });
 });
